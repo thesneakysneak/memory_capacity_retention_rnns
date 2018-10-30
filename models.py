b42d190d3950503d256cf63e09b7f4787c7ee8db
Danny Pretorius <danielp@blts.co.za>
2018-10-30 20:29:38 +0200

    Generic topology definition

--- models.py
+++ models.py
@@ -36,41 +36,46 @@ class ResetState(keras.callbacks.Callback):
         return
 
 
-def get_model(architecture=[2, 1, 1],
+def get_model(architecture=[2, 1, 1, 1],
               batch_size=10, timesteps=3,
               network_type="lstm",
               activation_function='tanh'):
     model = Sequential()
     # Hidden layer 1
+    return_sequences = False
+    if len(architecture) > 3:
+        return_sequences = True
     if network_type == "lstm":
-        model.add(LSTM(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]), stateful=True,
-                       unroll=True))
+        model.add(LSTM(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]),
+                       stateful=True, unroll=True, return_sequences=return_sequences))
     elif network_type == "gru":
-        model.add(GRU(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]), stateful=True,
-                      unroll=True))
+        model.add(GRU(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]),
+                      stateful=True, unroll=True, return_sequences=return_sequences))
     elif network_type == "elman_rnn":
         model.add(
-            SimpleRNN(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]), stateful=True,
-                      unroll=True))
+            SimpleRNN(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]),
+                      stateful=True, unroll=True, return_sequences=return_sequences))
+    elif network_type == "jordan_rnn":
+        model.add(
+            SimpleRNN(architecture[1], batch_input_shape=(batch_size, timesteps, architecture[0]),
+                      stateful=True, unroll=True, return_sequences=return_sequences))
 
     # Hidden layer how many ever
-    #         for h in range(2, len(architecture)-1):
-    #             print(h)
-    #             if network_type == "lstm":
-    #                 model.add(LSTM(32))
-    # #                 model.add(LSTM(units=architecture[h], activation=activation_function, stateful=True, unroll=True))
-    #             elif network_type == "gru":
-    #                 model.add(GRU(architecture[h], activation=activation_function, stateful=True, unroll=True))
-    #             elif network_type == "elman_rnn":
-    #                 model.add(SimpleRNN(architecture[h], activation=activation_function, stateful=True, unroll=True))
+    for h in range(2, len(architecture)-1):
+        print(h)
 
-    #         example_model = Sequential()
-    #         example_model.add(LSTM(architecture[1], return_sequences=True, stateful=True,
-    #         batch_input_shape=(batch_size, timesteps, architecture[0])))
+        return_sequences = False
+        if h < len(architecture)-2:
+            return_sequences = True
+        if network_type == "lstm":
+            model.add(LSTM(architecture[h], return_sequences=return_sequences))
 
-    #         example_model.add(LSTM(architecture[2], return_sequences=True, stateful=True))
+        elif network_type == "gru":
+            model.add(GRU(architecture[h], return_sequences=return_sequences))
+        elif network_type == "elman_rnn":
+            model.add(SimpleRNN(architecture[h], return_sequences=return_sequences))
 
-    #         example_model.add(Dense(architecture[-1], activation="softmax"))
+    model.add(Dense(architecture[-1], activation="softmax"))
     return model
 
 
@@ -86,9 +91,39 @@ def train_model(input_set, output_set, model, training_alg, batch_size):
     return model
 
 
+def test():
+    network_types = ["lstm", "gru", "elman_rnn", "jordan_rnn"]
+    activation_functions = ["tanh", "sigmoid", "elu",
+                            "relu", "exponential", "softplus",
+                            "softsign", "hard_sigmoid", "linear"]
+    architecture = []
+    import random
+    for i in range(100):
+        num_layers = random.randint(1, 5)
+        timesteps = random.randint(1, 300)
+        batch_size = random.randint(1, 30)
+        nn_index = random.randint(0, len(network_types) - 1)
+        activation_index = random.randint(0, len(activation_functions) - 1)
+        for i in range(num_layers):
+            nodes_in_layer = random.randint(3, 1000)
+            architecture.append(nodes_in_layer)
+        print("architecture",architecture,
+              "batch_size",batch_size,
+              "timesteps",timesteps,
+              "network_type", network_types[nn_index],
+              "activation_function", activation_functions[activation_index])
+        model = get_model(architecture=architecture,
+                          batch_size=batch_size,
+                          timesteps=timesteps,
+                          network_type=network_types[nn_index],
+                          activation_function=activation_functions[activation_index])
+        print(model.summary())
+
+
 earlystop = EarlyStopping(monitor='loss',  # loss
                           patience=10,
                           verbose=1,
                           min_delta=0.05,
                           mode='auto')
 reset_state = ResetState()
+test()
--- scratch_space/jordan_rnn.py
+++ scratch_space/jordan_rnn.py
@@ -1,57 +1,168 @@
+#
+# # LSTM for international airline passengers problem with time step regression framing
+# import numpy
+# import matplotlib.pyplot as plt
+# from keras import Model
+# from keras.utils import plot_model
+# from pandas import read_csv
+# import math
+# from keras.models import Sequential
+# from keras.layers import Dense, LSTMCell, add, TimeDistributed, Add, Concatenate
+# from keras.layers import LSTM, Recurrent
+# from sklearn.preprocessing import MinMaxScaler
+# from sklearn.metrics import mean_squared_error
+# from keras.layers import Input
+# import numpy as np
+# sample_input = np.array([[[0],[1]],[[0],[1]]])
+# sample_output = np.array([[0, 1, 0, 1], [0, 1, 0, 1]])
+#
+# #
+# #
+# #
+# # Define an input sequence and process it.
+# inputs = 5
+# num_layer2_outputs = 2
+# layer3_outputs = 2
+#
+# layer2_outputs = Input(shape=(None, num_layer2_outputs ), name='layer2_outputs')
+# dense_output = Input(shape=(None, layer3_outputs), name='layer3_outputs')
+#
+# # Layer 1
+#
+# layer1_inputs = Input(shape=(None, inputs), name='layer1_input')
+# layer1_prev_out = Concatenate(axis=-1)([layer1_inputs, layer2_outputs])
+#
+# layer1 = LSTM(7, return_state=True, return_sequences=True)
+# layer1_outputs, layer1_state_h, layer1_state_c = layer1(layer1_prev_out)
+#
+#
+# layer2_prev_out = layer1_outputs
+#
+# # layer2_prev_out = Concatenate()([layer1_outputs, dense_output])
+#
+# # Layer 2
+# layer2 = LSTM(num_layer2_outputs , return_state=True, return_sequences=True)
+# layer2_outputs, layer2_state_h, layer2_state_c = layer2(layer1)
+#
+# # Layer 2
+# layer3 = Dense(layer3_outputs, activation="softmax")
+# dense_output = layer3(layer2_outputs)
+#
+#
+# #
+# # Build model
+# #
+#
+# model = Sequential()
+# model.add(layer1)
+# model.add(layer2)
+# # model.add(layer3)
+#
+# model.compile(loss='categorical_crossentropy', optimizer='adam')
+#
+# input_set = np.array([[[1]*inputs + [0]*100], [[2]*inputs+ [0]*100], [[3]*inputs+ [0]*100]])
+# output_set =np.array( [[0]*layer3_outputs, [1]*layer3_outputs, [1]*layer3_outputs,])
+#
+# model.fit(input_set, output_set, epochs=2, batch_size=1,  verbose=2)
+# plot_model(model, show_shapes=True)
+
+
+
 
 # LSTM for international airline passengers problem with time step regression framing
 import numpy
 import matplotlib.pyplot as plt
+from keras import Model
+from keras.utils import plot_model
 from pandas import read_csv
 import math
 from keras.models import Sequential
-from keras.layers import Dense, LSTMCell, add, TimeDistributed
+from keras.layers import Dense, LSTMCell, add, TimeDistributed, Add, Concatenate, K
 from keras.layers import LSTM, Recurrent
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from keras.layers import Input
-
-input = Input((10,))
-readout_input = Input((10,))
-h_tm1 = Input((10,))
-c_tm1 = Input((10,))
-
-lstm_input = add([input, readout_input]) # Here we add to input.. you can do whatever you want with a Lambda layer
-
-output, h_t, c_t = LSTMCell(10)([lstm_input, h_tm1, c_tm1])
-
-rnn = Recurrent(input=input, initial_states=[h_tm1, c_tm1], output=output, final_states=[h_t, c_t], readout_input=readout_input)
-
-
+import numpy as np
+sample_input = np.array([[[0],[1]],[[0],[1]]])
+sample_output = np.array([[0, 1, 0, 1], [0, 1, 0, 1]])
 
 #
 #
 #
 # Define an input sequence and process it.
-inputs = 5
-layer2_outputs = 5
-layer3_outputs = 2
+num_inputs = 5
+num_layer2_outputs = 2
+
+dense_output = Input(shape=(None, num_layer2_outputs ), name='layer2_outputs')
 
 # Layer 1
-layer1_inputs = Input(shape=(None, inputs+layer2_outputs))
-layer1 = LSTM(7, return_state=True)
-layer1_outputs, layer1_state_h, layer1_state_c = layer1(layer1_inputs)
+layer1_inputs = Input(shape=(None, num_inputs), name='layer1_input')
+layer1_prev_out = Concatenate(axis=-1)([layer1_inputs, dense_output])
+
+layer1 = LSTM(7, return_state=True, return_sequences=True, name='layer1')
+layer1_outputs, layer1_state_h, layer1_state_c = layer1(layer1_prev_out)
 
 # Layer 2
-layer2_inputs = Input(shape=(None, layer1_outputs+layer3_outputs))
-layer2 = LSTM(layer2_outputs, return_state=True)
-layer2_outputs, layer2_state_h, layer2_state_c = layer2(layer2_inputs)
-
-# Layer 2
-dense_output = TimeDistributed(layer3_outputs, activation="softmax")
+output_nodes = Dense(num_layer2_outputs, activation="softmax", name='output_layer')
+dense_output = output_nodes(layer1_outputs)
 
 
 #
 # Build model
 #
 
-model = Sequential()
+model = Model([layer1_inputs], dense_output)
 model.add(layer1)
-model.add(layer2)
-model.add(dense_output)
+model.add(output_nodes)
+
 model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+input_set = np.array([[[1]*num_inputs], [[2]*num_inputs], [[3]*num_inputs]])
+output_set =np.array( [[0]*num_layer2_outputs, [1]*num_layer2_outputs, [1]*num_layer2_outputs])
+
+for epoc_ in range(10):
+    input_set = np.array([[[1] * num_inputs]])
+    try:
+        np_dense_output = K.eval(dense_output)
+    except:
+        np_dense_output = []
+        for i in range(input_set.shape[0]):
+            np_dense_output.append([[0]*num_layer2_outputs])
+    print(np_dense_output)
+    input_set_ = np.concatenate((input_set, np_dense_output), axis=-1)
+    model.fit(input_set_, output_set, epochs=1, verbose=2)
+plot_model(model, show_shapes=True)
+
+
+##############
+#   Encoder
+############
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.utils.vis_utils import plot_model
+# configure
+num_encoder_tokens = 71
+num_decoder_tokens = 93
+latent_dim = 256
+# Define an input sequence and process it.
+encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder = LSTM(latent_dim, return_state=True)
+encoder_outputs, state_h, state_c = encoder(encoder_inputs)
+# We discard `encoder_outputs` and only keep the states.
+encoder_states = [state_h, state_c]
+# Set up the decoder, using `encoder_states` as initial state.
+decoder_inputs = Input(shape=(None, num_decoder_tokens))
+# We set up our decoder to return full output sequences,
+# and to return internal states as well. We don't use the
+# return states in the training model, but we will use them in inference.
+decoder_lstm = LSTM(latent_dim, return_sequences=True, return_state=True)
+decoder_outputs, _, _ = decoder_lstm(decoder_inputs, initial_state=encoder_states)
+decoder_dense = Dense(num_decoder_tokens, activation='softmax')
+decoder_outputs = decoder_dense(decoder_outputs)
+# Define the model that will turn
+# `encoder_input_data` & `decoder_input_data` into `decoder_target_data`
+model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+# plot the model
+plot_model(model, to_file='model.png', show_shapes=True)
