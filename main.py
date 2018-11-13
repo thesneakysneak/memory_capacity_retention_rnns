241ba6cd8bb6c5692d90b8c9069d7edfa26112e7
Danny Pretorius <danielp@blts.co.za>
2018-11-13 19:14:42 +0200

    Rancher dammit man

--- main.py
+++ main.py
@@ -628,6 +628,9 @@ if __name__ == "__main__":
 
             import time
 
+            while True:
+                time.sleep(100)
+                print("================Still alive================")
         elif len(sys.argv[1:]) > 3:
             print("Experiment", sys.argv[1:])
             main(sys.argv[1:])
