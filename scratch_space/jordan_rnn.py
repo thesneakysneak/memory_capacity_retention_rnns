a8435a9afc968f5c77202987d7663c39bdb2f869
Danny Pretorius <danielp@blts.co.za>
2018-11-14 08:34:50 +0200

    I will revisit this later

--- scratch_space/jordan_rnn.py
+++ scratch_space/jordan_rnn.py
@@ -1,34 +1,77 @@
-
+import keras
 from keras import Model
 from keras.utils import plot_model
 
-from keras.layers import Dense, LSTMCell, add, TimeDistributed, Add, Concatenate, K
+from keras.layers import Dense, LSTMCell, add, TimeDistributed, Add, Concatenate, K, SimpleRNN
 from keras.layers import LSTM, Recurrent
 
+from keras.layers import ZeroPadding3D
 from keras.layers import Input
+from keras.layers import RNN
+
 import numpy as np
 sample_input = np.array([[[1, 1, 1, 1, 1],[0, 0]],[[1, 1, 1, 1, 1],[0, 0]]])
 sample_output = np.array([[0, 1, 0, 1], [0, 1, 0, 1]])
 
+
+class JordanRNNCell(keras.layers.Layer):
+
+    def __init__(self, units, model, **kwargs):
+        self.units = units
+        self.state_size = units
+        self.prev_model = model
+        self.states = None
+        super(JordanRNNCell, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.kernel = self.add_weight(shape=(input_shape[-1], self.units),
+                                      initializer='uniform',
+                                      name='kernel')
+        self.recurrent_kernel = self.add_weight(
+            shape=(self.units, self.units),
+            initializer='uniform',
+            name='recurrent_kernel')
+        self.built = True
+
+    def call(self, inputs, states):
+        if self.prev_model is not None and self.prev_model.states is not None:
+            prev_output = self.prev_model.states[0]
+        else:
+            prev_output = states[0]
+        h = K.dot(inputs, self.kernel)
+        output = h + K.dot(prev_output, self.recurrent_kernel)
+        self.states = output
+        return output, [output]
+
+# Layer2 = JordanRNNCell(32, None)
+# Layer1 = JordanRNNCell(10, Layer2)
+# cells = [Layer1, Layer2]
+# x = keras.Input((None, 5))
+# layer = RNN(cells)
+# y = layer(x)
 #
 #
 #
 # Define an input sequence and process it.
-def single_layer_jordan_rnn(num_inputs, num_output_layer_outputs, num_nodes_layer1, prev_model=None):
+def single_layer_jordan_rnn(num_inputs, num_output_layer_outputs, num_nodes_layer1, input_shape=(1,1,1), prev_model=None):
 
-    dense_output = Input(shape=(None, num_output_layer_outputs ))
+    layer1_inputs = Input(shape=(None, num_inputs), name='layer1_input')
+
+    if prev_model:
+        prev_output_layer = prev_model.layers[1].output
+    else:
+        prev_output_layer = ZeroPadding3D(padding=input_shape, data_format=None)
 
     # Layer 1
-    layer1_inputs = Input(shape=(None, num_inputs), name='layer1_input')
-    layer1_prev_out = Concatenate()([layer1_inputs, dense_output])
+    layer1_prev_out = Concatenate()([layer1_inputs, prev_output_layer])
 
-    layer1 = LSTM(num_nodes_layer1, return_state=True, return_sequences=True, name='layer1')
+    layer1 = Dense(num_nodes_layer1, return_state=True, return_sequences=True, name='layer1')
     layer1_outputs, layer1_state_h, layer1_state_c = layer1(layer1_prev_out)
 
     # Layer 2
     output_nodes = Dense(num_output_layer_outputs, activation="softmax", name='output_layer')(layer1_outputs)
 
-    model = Model([layer1_inputs, dense_output], output_nodes)
+    model = Model([layer1_inputs], output_nodes)
     return model
 
 def two_layer_jordan_rnn(num_inputs, num_layer2_outputs, num_output_layer_outputs, num_nodes_layer1, num_nodes_layer2):
