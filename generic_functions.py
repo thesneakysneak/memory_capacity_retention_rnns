1eec060d67c47f2d3a821edd13d8d400e69980eb
Danny Pretorius <danielp@blts.co.za>
2019-02-10 10:28:09 +0200

    Simplified restart functionality

--- generic_functions.py
+++ generic_functions.py
@@ -131,7 +131,7 @@ def train_test_neural_net_architecture(x_train, y_train,
     model.fit(x_train, y_train,
               validation_split=.2,
               callbacks=[reduce_lr, recurrent_models.earlystop2],
-              epochs=1000,
+              epochs=10,
               batch_size=batch_size,
               verbose=verbose)
     #
