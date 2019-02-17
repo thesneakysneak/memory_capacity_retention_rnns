8dd87fbaf870cd53cd027dc46611a56d513b8fa0
Danny Pretorius <danielp@blts.co.za>
2019-02-17 12:04:34 +0200

    Jordan RNN not checkpointing

--- main2.py
+++ main2.py
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         runner = 4
         runner = sys.argv[1:][0]
         print("runner ", runner )
-
+    for runner in range(1, 6):
         random.seed(1000)
         total_num_parameters = gf.divisible_by_all(15)
         print(total_num_parameters)
@@ -56,22 +56,22 @@ if __name__ == "__main__":
                           str([total_num_parameters[i]]).replace(" ", "").replace("[", "").replace("]", "") \
                           + ' ' + str(runner) + ' ' + str(i) + '" & '
             print("starting ", command_str)
-            os.system(command_str)
-            time.sleep(5)
+            # os.system(command_str)
+            # time.sleep(5)
 
             command_str = 'bash -c "python num_patterns.py ' + \
                           str([total_num_parameters[i]]).replace(" ", "").replace("[", "").replace("]", "") \
                           + ' ' + str(runner) + ' ' + str(i) + '" & '
             print("starting ", command_str)
-            os.system(command_str)
-            time.sleep(5)
+            # os.system(command_str)
+            # time.sleep(5)
 
             command_str = 'bash -c "python volume_experiment.py ' + \
                           str([total_num_parameters[i]]).replace(" ", "").replace("[", "").replace("]", "") \
                           + ' ' + str(runner) + ' ' + str(i) + '" & '
             print("starting ", command_str)
-            os.system(command_str)
+            # os.system(command_str)
 
-        while True:
-            time.sleep(10)
-            print("=============== STILL HERE ======================")
+        # while True:
+        #     time.sleep(10)
+        #     print("=============== STILL HERE ======================")
--- scratch_space/Jordan_rnn_2.py
+++ scratch_space/Jordan_rnn_2.py
@@ -3,152 +3,17 @@ import random
 import numpy
 import numpy as np
 from keras import Input, Model
-from keras.callbacks import ReduceLROnPlateau
-from keras.layers import SimpleRNN, Dense, Concatenate, SimpleRNNCell
+from keras.callbacks import ReduceLROnPlateau, ModelCheckpoint
+from keras.layers import SimpleRNN, Dense, Concatenate, SimpleRNNCell, constraints, regularizers, initializers
 import keras
 from keras.engine.base_layer import Layer
 from keras import backend as K
+from keras.layers.recurrent import _generate_dropout_mask
 from keras.legacy import interfaces
 from keras.engine.base_layer import InputSpec
+from tensorboard.backend.event_processing.event_file_inspector import FLAGS
+from tensorflow.python.keras.callbacks import TensorBoard
 
-class CustomDense(Dense):
-    """Just your regular densely-connected NN layer.
-    `Dense` implements the operation:
-    `output = activation(dot(input, kernel) + bias)`
-    where `activation` is the element-wise activation function
-    passed as the `activation` argument, `kernel` is a weights matrix
-    created by the layer, and `bias` is a bias vector created by the layer
-    (only applicable if `use_bias` is `True`).
-    Note: if the input to the layer has a rank greater than 2, then
-    it is flattened prior to the initial dot product with `kernel`.
-    # Example
-    ```python
-        # as first layer in a sequential model:
-        model = Sequential()
-        model.add(Dense(32, input_shape=(16,)))
-        # now the model will take as input arrays of shape (*, 16)
-        # and output arrays of shape (*, 32)
-        # after the first layer, you don't need to specify
-        # the size of the input anymore:
-        model.add(Dense(32))
-    ```
-    # Arguments
-        units: Positive integer, dimensionality of the output space.
-        activation: Activation function to use
-            (see [activations](../activations.md)).
-            If you don't specify anything, no activation is applied
-            (ie. "linear" activation: `a(x) = x`).
-        use_bias: Boolean, whether the layer uses a bias vector.
-        kernel_initializer: Initializer for the `kernel` weights matrix
-            (see [initializers](../initializers.md)).
-        bias_initializer: Initializer for the bias vector
-            (see [initializers](../initializers.md)).
-        kernel_regularizer: Regularizer function applied to
-            the `kernel` weights matrix
-            (see [regularizer](../regularizers.md)).
-        bias_regularizer: Regularizer function applied to the bias vector
-            (see [regularizer](../regularizers.md)).
-        activity_regularizer: Regularizer function applied to
-            the output of the layer (its "activation").
-            (see [regularizer](../regularizers.md)).
-        kernel_constraint: Constraint function applied to
-            the `kernel` weights matrix
-            (see [constraints](../constraints.md)).
-        bias_constraint: Constraint function applied to the bias vector
-            (see [constraints](../constraints.md)).
-    # Input shape
-        nD tensor with shape: `(batch_size, ..., input_dim)`.
-        The most common situation would be
-        a 2D input with shape `(batch_size, input_dim)`.
-    # Output shape
-        nD tensor with shape: `(batch_size, ..., units)`.
-        For instance, for a 2D input with shape `(batch_size, input_dim)`,
-        the output would have shape `(batch_size, units)`.
-    """
-
-    @interfaces.legacy_dense_support
-    def __init__(self, units,
-                 activation=None,
-                 use_bias=True,
-                 kernel_initializer='glorot_uniform',
-                 bias_initializer='zeros',
-                 kernel_regularizer=None,
-                 bias_regularizer=None,
-                 activity_regularizer=None,
-                 kernel_constraint=None,
-                 bias_constraint=None,
-                 **kwargs):
-        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
-            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
-        super(Dense, self).__init__(**kwargs)
-        self.units = units
-        self.activation = keras.activations.get(activation)
-        self.use_bias = use_bias
-        self.kernel_initializer = keras.initializers.get(kernel_initializer)
-        self.bias_initializer = keras.initializers.get(bias_initializer)
-        self.kernel_regularizer = keras.regularizers.get(kernel_regularizer)
-        self.bias_regularizer = keras.regularizers.get(bias_regularizer)
-        self.activity_regularizer = keras.regularizers.get(activity_regularizer)
-        self.kernel_constraint = keras.constraints.get(kernel_constraint)
-        self.bias_constraint = keras.constraints.get(bias_constraint)
-        self.input_spec = InputSpec(min_ndim=2)
-        self.supports_masking = True
-
-
-    def build(self, input_shape):
-        assert len(input_shape) >= 2
-        input_dim = input_shape[-1]
-
-        self.kernel = self.add_weight(shape=(input_dim, self.units),
-                                      initializer=self.kernel_initializer,
-                                      name='kernel',
-                                      regularizer=self.kernel_regularizer,
-                                      constraint=self.kernel_constraint)
-        if self.use_bias:
-            self.bias = self.add_weight(shape=(self.units,),
-                                        initializer=self.bias_initializer,
-                                        name='bias',
-                                        regularizer=self.bias_regularizer,
-                                        constraint=self.bias_constraint)
-        else:
-            self.bias = None
-        self.input_spec = InputSpec(min_ndim=2, axes={-1: input_dim})
-        self.built = True
-
-    def call(self, inputs):
-        self.output = K.dot(inputs, self.kernel)
-        if self.use_bias:
-            self.output = K.bias_add(self.output, self.bias, data_format='channels_last')
-        if self.activation is not None:
-            self.output = self.activation(self.output)
-        return self.output
-
-    def compute_output_shape(self, input_shape):
-        assert input_shape and len(input_shape) >= 2
-        assert input_shape[-1]
-        output_shape = list(input_shape)
-        output_shape[-1] = self.units
-        return tuple(output_shape)
-
-    def get_config(self):
-        config = {
-            'units': self.units,
-            'activation': keras.activations.serialize(self.activation),
-            'use_bias': self.use_bias,
-            'kernel_initializer': keras.initializers.serialize(self.kernel_initializer),
-            'bias_initializer': keras.initializers.serialize(self.bias_initializer),
-            'kernel_regularizer': keras.regularizers.serialize(self.kernel_regularizer),
-            'bias_regularizer': keras.regularizers.serialize(self.bias_regularizer),
-            'activity_regularizer':
-                keras.regularizers.serialize(self.activity_regularizer),
-            'kernel_constraint': keras.constraints.serialize(self.kernel_constraint),
-            'bias_constraint': keras.constraints.serialize(self.bias_constraint)
-        }
-        base_config = super(Dense, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
-
-    def get_output(self):
-        return self.output
 
 class SimpleJordanRNNCell(SimpleRNNCell):
     """Cell class for SimpleRNN.
@@ -246,7 +111,7 @@ class SimpleJordanRNNCell(SimpleRNNCell):
 
         if self.next_layer != None:
             self.output_layer_kernel = self.add_weight(shape=(self.next_layer.shape[-1].value, self.units),
-                                          name='kernel',
+                                          name='jordan_kernel',
                                           initializer=self.kernel_initializer,
                                           regularizer=self.kernel_regularizer,
                                           constraint=self.kernel_constraint)
@@ -298,7 +163,7 @@ class SimpleJordanRNNCell(SimpleRNNCell):
 
         # Jordan specific output calculation
         if self.next_layer != None:
-            print("Jordan activate", prev_output.shape, self.output_layer_kernel.shape, self.next_layer.shape)
+            # print("Jordan activate", prev_output.shape, self.output_layer_kernel.shape, self.next_layer.shape)
             output = h + K.dot(prev_output, self.recurrent_kernel) + keras.backend.batch_dot(self.next_layer, self.output_layer_kernel, axes=None) #K.dot()
         else:
             print("Jordan not activate")
@@ -343,9 +208,9 @@ class SimpleJordanRNNCell(SimpleRNNCell):
 
 num_inputs = 1
 num_output_layer_outputs = 1
-x = [random.random() for i in range(50)]
-y = [random.random() for i in range(50)]
-
+x = [random.random() for i in range(10)]
+y = [random.random() for i in range(10)]
+x =y
 
 x = numpy.array(x).reshape(-1, 1, 1).astype(np.float32)
 y = numpy.array(y).reshape(-1, 1).astype(np.float32)
@@ -354,11 +219,11 @@ num_cells_in_hidden_layer = 10
 
 input_layer = keras.Input((None, num_inputs))
 
-n = K.variable([[1.0]])
+n = K.variable([[-0.0]])
 
 output_layer = keras.Input(tensor=n)
 
-cells = [SimpleJordanRNNCell(num_inputs, next_layer=output_layer) for _ in range(num_cells_in_hidden_layer )]
+cells = [SimpleJordanRNNCell(num_inputs, next_layer=output_layer, activation="elu") for _ in range(num_cells_in_hidden_layer )]
 
 layer = keras.layers.RNN(cells)
 hidden_layer = layer(input_layer)
@@ -368,12 +233,25 @@ for i in cells:
     i.next_layer = output_layer
 
 
+
 model = Model([input_layer], output_layer)
 
-model.compile(loss='mse', optimizer='adam')
-model.fit(x, y, epochs=100, verbose=2)
+reduce_lr = ReduceLROnPlateau(monitor='loss', factor=0.1,
+                              patience=20, min_lr=0.000000001, verbose=1, cooldown=1)
+
+model.compile(loss='mse', optimizer='adam',  metrics=['accuracy'])
+
+callbacks = [
+		ModelCheckpoint(filepath="/home/danielp/Documents/Masters/Code/memory_capacity_retention_rnns/scratch_space/weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5",
+                        monitor="loss", verbose=1, save_best_only=False),
+		ReduceLROnPlateau(monitor='loss', factor=0.5, patience=2)
+	]
+
+model.fit(x, y, epochs=1000, verbose=1, callbacks=[reduce_lr])
 
 
-
+y_predict = model.predict(x)
+for i in range(len(y)):
+    print(y_predict[i], y[i])
 
 
